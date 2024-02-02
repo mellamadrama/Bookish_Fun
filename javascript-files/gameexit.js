c46d02cfefb576febafbf6d1e366bbaf91ea994a
mellamadrama <s10258027@connect.np.edu.sg>
2024-02-02 23:11:31 +0800

    edited the js

---
+++ javascript-files/gameexit.js
@@ -0,0 +1,12 @@
+import { db } from "./firebase.js";
+import { doc, getDoc } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js'
+
+let score = 0;
+const acc = localStorage.getItem("accounts");
+const pointsSystem = doc(db, "accounts", acc);
+
+getDoc(pointsSystem).then(function (data){
+  score = data.data().score;
+  document.getElementById("score").innerText = score
+});
+
